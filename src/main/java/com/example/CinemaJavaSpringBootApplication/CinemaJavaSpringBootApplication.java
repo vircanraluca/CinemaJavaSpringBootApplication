041ed4fcffea748b4cf280040d03ee7bc06f206b
Raluca <vircan.raluca@gmail.com>
2026-03-01 20:22:25 +0200

    Initial commit - Cinema App

---
+++ src/main/java/com/example/CinemaJavaSpringBootApplication/CinemaJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaJavaSpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaJavaSpringBootApplication/CinemaJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaJavaSpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
